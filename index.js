1aa3e9badcd6cb14949fcbb15010709c02dd055a
Jakub Kalinowski <j.kalinowski.86@gmail.com>
2016-03-17 22:08:55 +0000

    Part 1b complete

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ var fs = require('fs') ;
 var http = require('http');
 var requestListener = function (req, res) {
   res.writeHead(200);
-var data = fs.readFileSync("bitstarter.html");
+
+fs.readFile("bitstarter.html", 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
   res.end(data);
+});
+
 }
 
 var server = http.createServer(requestListener);
 server.listen(process.env.PORT || 8080);
+
+
