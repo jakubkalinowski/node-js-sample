456a545325875ecbd5a34872902cfe564b480406
Ubuntu <ubuntu@ip-172-31-46-164.us-west-2.compute.internal>
2016-03-17 21:36:12 +0000

    heroku initial setup

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-var express = require('express')
-var app = express()
+var http = require('http');
+var requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('Hello, World!\n');
+}
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
-
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+var server = http.createServer(requestListener);
+server.listen(process.env.PORT || 8080);
